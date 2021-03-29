70539623528d6035f04ecadb9f8fc9e61775f9eb
qingqingqingli <kadlqbetty@gmail.com>
2021-03-29 17:34:54 +0200

    update: jest mock setting

--- __tests__/PostScreen-test.js
+++ __tests__/PostScreen-test.js
@@ -1,20 +1,7 @@
 import * as React from "react";
-import { render, act, fireEvent } from "@testing-library/react-native";
+import { render, act } from "@testing-library/react-native";
 import App from "../App";
 import renderer from "react-test-renderer";
-
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        userId: 1,
-        id: 1,
-        title:
-          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipito",
-      }),
-  })
-);
 
 describe("Testing Post Screen", () => {
   test("Can render correctly", async () => {
